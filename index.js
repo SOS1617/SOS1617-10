c3e24583cf3e2a50f4ae186e9f52a9150116b6a6
sos161710dbc <david.bote.cabeza@gmail.com>
2017-02-28 15:59:44 +0000

    Index completo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = (process.env.PORT || 16778);
 var date = new Date();
 var app = express();
 
-//
+//Return the current day and time of the server in the format: "1st March of 2017, 08:35:00"
 app.get("/time",(req,res) => {
    res.send("<html><h1>"+format(date, "UTC:dS mmmm 'of' yyyy, HH:MM:ss")+"</h1></html>"); 
 });
