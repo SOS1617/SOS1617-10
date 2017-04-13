df08b98b2fb3653f9a0d763a254289d46317527a
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-13 10:27:45 +0000

    updated added

--- public/beers/list-ctrl.js
+++ public/beers/list-ctrl.js
@@ -31,7 +31,7 @@ angular
         };
         $scope.updateBeer = function(){
             var newbeer = $scope.newBeer;
-            $http.put(url+"/api/v1/beers-stats/"+newbeer.country+"/"+newbeer.birthyear+"?apikey=sos1617-jesusguerre",$scope.newBeer).then(function (response){
+            $http.put(url+"/api/v1/beers-stats/"+newbeer.country+"/"+newbeer.birthyear+"?apikey=sos1617-jesusguerre",newbeer).then(function (response){
                 console.log("Beer updated");
                 refresh();
                 
