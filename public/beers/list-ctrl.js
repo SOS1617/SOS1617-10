668e19c451290cff1a6be062d3ccf0d076eb84b0
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-13 11:48:33 +0000

    delete all added

--- public/beers/list-ctrl.js
+++ public/beers/list-ctrl.js
@@ -2,11 +2,12 @@ angular
     .module("BeersManagerApp")
     .controller("ListCtrl", ["$scope", "$http", function($scope, $http) { //inyeccion de dependencia
         var url = "http://sos1617-10.herokuapp.com";
+        var apikey="apikey="+$scope.apikey;
         console.log("Controller initialized right");
         refresh();
         function refresh(){
         $http
-            .get(url+"/api/v1/beers-stats?apikey=sos1617-jesusguerre")
+            .get(url+"/api/v1/beers-stats?"+apikey)
             .then(function(response) {
                 $scope.beers = response.data;
             });
@@ -14,7 +15,7 @@ angular
         $scope.addBeer = function() {
             $scope.Beer.birthyear=Number($scope.Beer.birthyear);
             $http
-                .post(url+"/api/v1/beers-stats?apikey=sos1617-jesusguerre", $scope.Beer)
+                .post(url+"/api/v1/beers-stats?"+apikey, $scope.Beer)
                 .then(function(response) {
                     console.log("Beer added"+$scope.Beer.name);
                     refresh();
@@ -23,7 +24,7 @@ angular
         };
         $scope.deleteBeer = function(country,birthyear){
             $http
-                .delete(url+"/api/v1/beers-stats/"+country+"/"+birthyear+"?apikey=sos1617-jesusguerre").then(function (response){
+                .delete(url+"/api/v1/beers-stats/"+country+"/"+birthyear+"?"+apikey).then(function (response){
                     console.log("Beer deleted");
                     refresh();
                 }
@@ -32,7 +33,7 @@ angular
         };
         $scope.updateBeer = function(country,birthyear){
             $scope.Beer.birthyear=Number($scope.Beer.birthyear);
-            $http.put(url+"/api/v1/beers-stats/"+country+"/"+Number(birthyear)+"?apikey=sos1617-jesusguerre",$scope.Beer).then(function (response){
+            $http.put(url+"/api/v1/beers-stats/"+country+"/"+Number(birthyear)+"?"+apikey,$scope.Beer).then(function (response){
                 console.log("Beer updated", $scope.Beer);
                 refresh();
                 
@@ -43,7 +44,7 @@ angular
         };
          $scope.deleteBeers = function(){
             $http
-                .delete(url+"/api/v1/beers-stats/?apikey=sos1617-jesusguerre").then(function (response){
+                .delete(url+"/api/v1/beers-stats/?"+apikey).then(function (response){
                     console.log("Beers deleted");
                     refresh();
                 }
@@ -52,7 +53,7 @@ angular
         };
         $scope.fillfields = function(country,birthyear){
             $scope.Beer = $http
-            .get(url+"/api/v1/beers-stats/"+country+"/"+birthyear+"?apikey=sos1617-jesusguerre")
+            .get(url+"/api/v1/beers-stats/"+country+"/"+birthyear+"?"+apikey)
             .then(function(response) {
                 $scope.Beer = response.data;
                 delete $scope.Beer["_id"];
