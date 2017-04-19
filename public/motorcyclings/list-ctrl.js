ef0d55d81cb32df91b9910fe4c316a64525bbf63
sos161710dbc <david.bote.cabeza@gmail.com>
2017-04-19 14:42:03 +0000

    Motorcycling fixed

--- public/motorcyclings/list-ctrl.js
+++ public/motorcyclings/list-ctrl.js
@@ -3,10 +3,15 @@ angular
 .controller("ListCtrl",["$scope","$http",function($scope,$http){
     console.log("Controller initialized (splited right)");
     var url = "http://sos1617-10.herokuapp.com/api/v1";
+    var apikey = "";
+    var yearfrom = "";
+    var yearto = "";
+    var limit = "";
+    var offset = "";
     
     function refresh(){
         $http
-            .get(url + "/motorcycling-stats/?apikey=davbotcab")
+            .get(url + "/motorcycling-stats/?" + apikey + yearfrom + yearto + limit + offset)
             .then(function(response){
                 $scope.motorcyclings = response.data;
             });
@@ -15,7 +20,7 @@ angular
     
     $scope.loadInitialData = function() {
         $http
-            .get(url + "/motorcycling-stats/loadInitialData?apikey=davbotcab")
+            .get(url + "/motorcycling-stats/loadInitialData?"  + apikey)
             .then(function(response) {
                 console.log("Carga inicial");
                 refresh();
@@ -24,7 +29,7 @@ angular
         
     $scope.addMotorcycling = function(){
         $http
-            .post(url + "/motorcycling-stats?apikey=davbotcab", $scope.newMotorcycling)
+            .post(url + "/motorcycling-stats?"  + apikey, $scope.newMotorcycling)
             .then(function(response){
                 console.log("Motorcycling added");
                 refresh();
@@ -34,7 +39,7 @@ angular
     
     $scope.deleteMotorcycling = function(country, year) {
         $http
-            .delete(url + "/motorcycling-stats/" + country + "/" + year + "?apikey=davbotcab")
+            .delete(url + "/motorcycling-stats/" + country + "/" + year + "?"  + apikey)
             .then(function(response) {
                 console.log("Motorcycling deleted");
                 refresh();
@@ -43,22 +48,55 @@ angular
 
     $scope.deleteAllMotorcyclings = function() {
         $http
-            .delete(url + "/motorcycling-stats?apikey=davbotcab")
+            .delete(url + "/motorcycling-stats?"  + apikey)
             .then(function(response) {
                 console.log("All motorcyclings deleted");
                 refresh();
             });
     };
     
+    
     $scope.putMotorcycling = function(){
     $scope.newMotorcycling.year=Number( $scope.newMotorcycling.year);
         $http
-            .put(url +"/motorcycling-stats/"+ $scope.newMotorcycling.country + "/" +  $scope.newMotorcycling.year + "?apikey=davbotcab", $scope.newMotorcycling )
+            .put(url +"/motorcycling-stats/"+ $scope.newMotorcycling.country + "/" +  $scope.newMotorcycling.year + "?"  + apikey, $scope.newMotorcycling )
             .then(function(response){
                 console.log("Motorcycling Updated");
                 refresh();
             });
     };
     
- refresh();
+    $scope.sendapi = function() {
+        apikey = "apikey=" + $scope.apikeyfield;
+        refresh();
+    };
+    
+    $scope.filter = function(YearFrom, YearTo, Limit, Offset) {
+            if (YearFrom) {
+                yearfrom = "&from=" + Number(YearFrom);
+            }
+            else {
+                yearfrom = "";
+            }
+            if (YearTo) {
+                yearto = "&to=" + Number(YearTo);
+            }
+            else {
+                yearto = "";
+            }
+            if (Limit) {
+                limit = "&limit=" + Number(Limit);
+            }
+            else {
+                limit = "";
+            }
+            if (Offset) {
+                offset = "&offset=" + Number(Offset);
+            }
+            else {
+                offset = "";
+            }
+            refresh();
+
+        };
 }]);
