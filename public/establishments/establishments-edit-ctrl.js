69abbd76637dcf0bb77f662810031e8c79c004c6
sos161710nto <nuria.t.ojeda@gmail.com>
2017-04-24 20:18:34 +0000

    page

--- public/establishments/establishments-edit-ctrl.js
+++ public/establishments/establishments-edit-ctrl.js
@@ -10,14 +10,15 @@ angular
             $http
                 .get(url + "/establishments/" + $routeParams.country + "/" + year + "?" + apikey)
                 .then(function(response) { //promesas
-                    $scope.newEstablishment = response.data;
+                    $scope.updatedEstablishment = response.data;
+                    delete $scope.updatedEstablishment["_id"];
                 });
         }
         
         $scope.updateEstablishment = function() {
             var year = Number($routeParams.year);
             $http
-                .put(url + "/establishments/" + $routeParams.country + "/" + year + "?" + apikey, $scope.newEstablishment)
+                .put(url + "/establishments/" + $routeParams.country + "/" + year + "?" + apikey, $scope.updatedEstablishment)
                 .then(function(response){
                     console.log("Establishment updated");
                     $location.path("/");
