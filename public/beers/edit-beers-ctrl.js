53c5ad6c8c6ef40f0619f04b6126e49fe0db4153
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-24 19:56:47 +0000

    err

---
+++ public/beers/edit-beers-ctrl.js
@@ -0,0 +1,24 @@
+angular
+    .module("ContactManagerApp")
+    .controller("EditBeersCtrl", ["$scope", "$http","$routeParams","$location", 
+    function($scope, $http,$routeParams,$location) { //inyeccion de dependencia
+        
+        console.log("Edit Controller initialized");
+        
+        refresh();
+        function refresh(){
+        $http
+            .get("/api/v1/beers-stats/"+$routeParams.country+"/"+$routeParams.birthyear+'apikey="sos1617-jesusguerre"')
+            .then(function(response) {
+                $scope.Beer = response.data;
+            });
+        }
+        $scope.updateBeer = function (country,birthyear) {
+          $http.put("/api/v1/beers-stats/"+ country + "/" + Number(birthyear)+ 'apikey=sos1617-jesusguerre',$scope.Beer).then( function (response){
+             $location.path("/beers");
+          });  
+        };
+       
+        
+        
+    }]);
