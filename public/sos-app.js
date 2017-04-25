ff78d24883eb66d590b6f2c3ab84a6d3a4dd297d
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-25 21:58:09 +0000

    index.html

--- public/sos-app.js
+++ public/sos-app.js
@@ -1,22 +1,25 @@
-angular.module("SOS161710",["ngRoute"]).config(function ($routeProvider){
-    $routeProvider.when("/",{
-        templateUrl : "/home.html"
-    }).when("/beers/",{
-        templateUrl : "/beers/BeersList.html",
-        controller : "BeersCtrl"
-        
-    }).when("/beers/updateBeer/:country/:birthyear",{
-       templateUrl : "/beers/EditBeers.html",
-       controller : "EditBeersCtrl"
-       
-    }).when("/establishments/",{
-        templateUrl : "/establishments/establishments-list.html",
-        controller : "EstablishmentsCtrl"
-        
-    }).when("/establishments/updateEstablishment/:country/:year",{
-       templateUrl : "/establishments/establishments-edit.html",
-       controller : "EstablishmentsEditCtrl"
-       
+angular.module("SOS161710", ["ngRoute"]).config(function($routeProvider) {
+    $routeProvider.when("/:params", {
+        templateUrl: "/home.html"
+        controller: function($scope, $stateParams) {
+            $scope.portfolioId = $stateParams.portfolioId;
+        }
+    }).when("/beers/", {
+        templateUrl: "/beers/BeersList.html",
+        controller: "BeersCtrl"
+
+    }).when("/beers/updateBeer/:country/:birthyear", {
+        templateUrl: "/beers/EditBeers.html",
+        controller: "EditBeersCtrl"
+
+    }).when("/establishments/", {
+        templateUrl: "/establishments/establishments-list.html",
+        controller: "EstablishmentsCtrl"
+
+    }).when("/establishments/updateEstablishment/:country/:year", {
+        templateUrl: "/establishments/establishments-edit.html",
+        controller: "EstablishmentsEditCtrl"
+
     });
 });
-            console.log("App initialized");
+console.log("App initialized");
