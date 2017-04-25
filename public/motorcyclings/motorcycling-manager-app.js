8f7945d40f7500dce683ffc37ae362707bacd2cf
sos161710dbc <david.bote.cabeza@gmail.com>
2017-04-25 22:10:14 +0000

    Motorcycling fixed

--- public/motorcyclings/motorcycling-manager-app.js
+++ public/motorcyclings/motorcycling-manager-app.js
@@ -4,10 +4,6 @@ angular.module("MotorcyclingsManagerApp",["ngRoute"]).config(function($routeProv
         .when("/", {
             templateUrl:"motorcyclings-list.html",
             controller:"MotorcyclingsCtrl"
-        })
-        .when("/motorcycling/:country/:year", {
-            templateUrl:"motorcyclings-edit.html",
-            controller:"MotorcyclingsEditCtrl"
         });
 
     console.log("App initialized and configured");
