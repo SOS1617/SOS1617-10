a88bc2dd03d81315061cb0c97b7d611f2deca47b
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-25 14:46:11 +0000

    1 app

--- public/sos-app.js
+++ public/sos-app.js
@@ -1,7 +1,6 @@
 angular.module("SOS161710",["ngRoute"]).config(function ($routeProvider){
     $routeProvider.when("/",{
-        templateUrl : "/home.html",
-        controller : "BeersCtrl"
+        templateUrl : "/home.html"
     }).when("/beers/",{
         templateUrl : "/beers/BeersList.html",
         controller : "BeersCtrl"
