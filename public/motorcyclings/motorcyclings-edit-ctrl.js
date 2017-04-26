7e4a84f690e8f3ff70c1e4d598e469f3d7dfcf5d
sos161710dbc <david.bote.cabeza@gmail.com>
2017-04-26 07:52:03 +0000

    Motorcycling fixed 3

--- public/motorcyclings/motorcyclings-edit-ctrl.js
+++ public/motorcyclings/motorcyclings-edit-ctrl.js
@@ -1,6 +1,6 @@
 angular
 .module("MotorcyclingsManagerApp")
-.controller("MotorcyclingsCtrl",["$scope","$http","$routeParams","$location",function($scope,$http,$routeParams,$location){
+.controller("MotorcyclingsEditCtrl",["$scope","$http","$routeParams","$location",function($scope,$http,$routeParams,$location){
     console.log("Edit controller initialized");
     var url = "http://sos1617-10.herokuapp.com/api/v1";
     var apikey = "";
@@ -8,9 +8,10 @@ angular
     function refresh(){
         var year = Number($routeParams.year);
         $http
-            .get(url + "/motorcycling-stats/?" + $routeParams.country + "/" + year + "?" + apikey)
+            .get(url + "/motorcycling-stats/?" + $routeParams.country + "/" + $routeParams.year + "?" + apikey)
             .then(function(response){
                 $scope.motorcyclings = response.data;
+                delete $scope.updatedMotorcycling["_id"];
             });
     }
     
@@ -20,7 +21,17 @@ angular
             .put(url +"/motorcycling-stats/"+ $routeParams.country + "/" + year + "?" + apikey, $scope.updatedMotorcycling)
             .then(function(response){
                 console.log("Motorcycling Updated");
+                bootbox.alert("Motorcycling Updated");
                 $location.path("/");
+            }, function(response) {
+                    switch (response.status) {
+                        case 400:
+                            bootbox.alert("Bad Request. Please enter all fields correctly.");
+                            break;
+                        default:
+                            bootbox.alert("Please make sure that you have entered all the fields");
+                            break;
+                    }
             });
     };
     
