419c15bc83ebe5d42efee19e1cde2c12bd2c0637
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-29 15:45:38 +0000

    c3 installed

--- public/beers/graph.js
+++ public/beers/graph.js
@@ -2,16 +2,38 @@ angular
     .module("SOS161710")
     .controller("BeersGraphs", ["$http", function() {
         console.log("Controller initialized");
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: [
-                    ['data1', 30, 200, 100, 400, 150, 250],
-                    ['data2', 50, 20, 10, 40, 15, 25]
-                ]
+         var beersbycountry = [];
+         var url="http://sos1617-10.herokuapp.com/api/v2/beers-stats";
+         var apikey = "jesusguerre";
+        $http.get(url+apikey).then(function (response){
+            var countries = new Set ([response.data.map( function (x) {return x.country})]);
+            for (var country in countries){
+                beersbycountry.push(getFromApi(country));
             }
+            
         });
 
+function getFromApi (country){
+    var response;
+     $http.get(url+"/"+country+apikey).then(function (response) {
+        response= [country,response.data.length];
+    });
+    return response;
+}
+
+
+var chart = c3.generate({
+   
+    
+    
+    data: {
+        columns: [
+           beersbycountry
+        ],
+        type : 'pie',
+       
+    }
+});
 
 
 
