3db3b8c0aabeca4bcd36561c4093c5f71f6570b5
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-04-29 15:47:27 +0000

    c3 installed

--- public/beers/graph.js
+++ public/beers/graph.js
@@ -5,7 +5,7 @@ angular
          var beersbycountry = [];
          var url="http://sos1617-10.herokuapp.com/api/v2/beers-stats";
          var apikey = "jesusguerre";
-        $http.get(url+apikey).then(function (response){
+        $http.get(url+"/?"+apikey).then(function (response){
             var countries = new Set ([response.data.map( function (x) {return x.country})]);
             for (var country in countries){
                 beersbycountry.push(getFromApi(country));
@@ -15,7 +15,7 @@ angular
 
 function getFromApi (country){
     var response;
-     $http.get(url+"/"+country+apikey).then(function (response) {
+     $http.get(url+"/"+country+"?"+apikey).then(function (response) {
         response= [country,response.data.length];
     });
     return response;
