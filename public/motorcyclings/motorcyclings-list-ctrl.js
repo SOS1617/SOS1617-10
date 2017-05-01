215504f43850634f44e5f32170bdca5de50147e8
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-01 14:28:43 +0000

    Motorcycling pagination

--- public/motorcyclings/motorcyclings-list-ctrl.js
+++ public/motorcyclings/motorcyclings-list-ctrl.js
@@ -135,29 +135,6 @@ angular
 
         };
     
-    
-    //Paginación
-    $scope.viewby = 10;
-    $scope.totalItems = function() {
-        return $scope.data.length;
-    };
-    $scope.currentPage = 1;
-    $scope.itemsPerPage = function() {
-        return $scope.limit;
-    };
-    $scope.maxSize = 5; //Number of pager buttons to show
 
-    $scope.setPage = function(pageNo) {
-        $scope.currentPage = pageNo;
-    };
-
-    $scope.pageChanged = function() {
-        console.log('Page changed to: ' + $scope.currentPage);
-    };
-
-    $scope.setItemsPerPage = function(num) {
-        $scope.itemsPerPage = num;
-        $scope.currentPage = 1; //reset to first paghe
-    };
     
 }]);
