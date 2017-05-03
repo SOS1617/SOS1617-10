bbb0de5db9cde38b0688f68fa5e7b53533b71936
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-03 17:30:23 +0000

    Motorcycling pagination

--- public/motorcyclings/motorcyclings-list-ctrl.js
+++ public/motorcyclings/motorcyclings-list-ctrl.js
@@ -149,7 +149,7 @@ angular
 
         };
     
-    $scope.setPage = function(pageNumber) {
+ /*   $scope.setPage = function(pageNumber) {
         $scope.currentPage = pageNumber;
 
         if (pageNumber == 1) {
@@ -168,6 +168,31 @@ angular
         offset = "&offset=" + (($scope.currentPage * size) - size);
 
         refresh();
-    };
+    }; */
+  $scope.currentPage = 0;
+  $scope.itemsPerPage = 5;
+  $scope.items = [];
+  for(var i=0; i<25; i++){
+    $scope.items.push('Product ' + i);
+  }
+  $scope.firstPage = function() {
+    return $scope.currentPage == 0;
+  }
+  $scope.lastPage = function() {
+    var lastPageNum = Math.ceil($scope.items.length / $scope.itemsPerPage - 1);
+    return $scope.currentPage == lastPageNum;
+  }
+  $scope.numberOfPages = function(){
+    return Math.ceil($scope.items.length / $scope.itemsPerPage);
+  }
+  $scope.startingItem = function() {
+    return $scope.currentPage * $scope.itemsPerPage;
+  }
+  $scope.pageBack = function() {
+    $scope.currentPage = $scope.currentPage - 1;
+  }
+  $scope.pageForward = function() {
+    $scope.currentPage = $scope.currentPage + 1;
+  }
     
 }]);
