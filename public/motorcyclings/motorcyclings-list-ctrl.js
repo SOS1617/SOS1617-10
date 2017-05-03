654bff58916c65f198bcd5983194e9dcef5a899f
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-03 17:52:53 +0000

    Motorcycling pagination

--- public/motorcyclings/motorcyclings-list-ctrl.js
+++ public/motorcyclings/motorcyclings-list-ctrl.js
@@ -1,5 +1,11 @@
 angular
 .module("SOS161710")
+.filter('startFrom', function(){
+  return function(input, start){
+    start = +start;
+    return input.slice(start);
+  }
+})
 .controller("MotorcyclingsCtrl",["$scope","$http","$rootScope",function($scope,$http,$rootScope){
     console.log("Controller initialized");
     var url = "http://sos1617-10.herokuapp.com/api/v2";
@@ -172,9 +178,11 @@ angular
         refresh();
     }; */
 
-  
+    $scope.startingItem = function() {
+        return $scope.currentPage * size;
+    }
     $scope.firstPage = function() {
-        return $scope.currentPage == 1;
+        return $scope.currentPage == 0;
     }
     $scope.lastPage = function() {
         return $scope.currentPage == $scope.lastPageNum;
