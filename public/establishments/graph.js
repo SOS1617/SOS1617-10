63afe97da41de8496b8d25a6d7e68d2737909207
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-04 16:43:00 +0000

    graph

--- public/establishments/graph.js
+++ public/establishments/graph.js
@@ -8,14 +8,50 @@ angular
         var url = "http://sos1617-10.herokuapp.com/api/v2/establishments";
         var apikey = "apikey=nurtrioje";
         $http.get(url + "/?" + apikey).then(function(response) {
-            TESTER = document.getElementById('tester');
-            Plotly.plot(TESTER, [{
-                x: ['a', 'b', 'c', 'd', 'e'],
-                y: [1, 2, 4, 8, 16]
-            }], {
-                margin: {
-                    t: 0
+            Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv', function(err, rows) {
+                var YEAR = 2007;
+                var continents = ['Asia', 'Europe', 'Africa', 'Oceania', 'Americas'];
+                var POP_TO_PX_SIZE = 2e5;
+
+                function unpack(rows, key) {
+                    return rows.map(function(row) {
+                        return row[key];
+                    });
                 }
+
+                var data = continents.map(function(continent) {
+                    var rowsFiltered = rows.filter(function(row) {
+                        return (row.continent === continent) && (+row.year === YEAR);
+                    });
+                    return {
+                        mode: 'markers',
+                        name: continent,
+                        x: unpack(rowsFiltered, 'lifeExp'),
+                        y: unpack(rowsFiltered, 'gdpPercap'),
+                        text: unpack(rowsFiltered, 'country'),
+                        marker: {
+                            sizemode: 'area',
+                            size: unpack(rowsFiltered, 'pop'),
+                            sizeref: POP_TO_PX_SIZE
+                        }
+                    };
+                });
+                var layout = {
+                    xaxis: {
+                        title: 'Life Expectancy'
+                    },
+                    yaxis: {
+                        title: 'GDP per Capita',
+                        type: 'log'
+                    },
+                    margin: {
+                        t: 20
+                    },
+                    hovermode: 'closest'
+                };
+                Plotly.plot('my-graph', data, layout, {
+                    showLink: false
+                });
             });
         });
     }]);
