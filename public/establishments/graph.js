98101875604a52aa9c0e54576626d903c4583d69
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-04 19:26:39 +0000

    graph

--- public/establishments/graph.js
+++ public/establishments/graph.js
@@ -128,7 +128,24 @@ angular
             }
 
             //************************* HIGHCHARTS *************************//
-
+            
+            var establishmentsSeries = [];
+            establishmentsSeries.push('{data: [');
+            response.data.forEach((e) => {
+                establishmentsSeries.push({x: e.beds, y: e.nights, z: e.number, name: e.country, country: e.country});
+            });
+            establishmentsSeries.push(']}');
+            
+            var averageBeds;
+            var averageNights;
+            
+            response.data.forEach((d) => {
+                averageBeds += d.beds;
+                averageNights += d.nights;
+            });
+            averageBeds = averageBeds/response.data.length;
+            averageNights = averageNights/response.data.length;
+            
             Highcharts.chart('establishmentsHighchart', {
 
                 chart: {
@@ -142,33 +159,30 @@ angular
                 },
 
                 title: {
-                    text: 'Sugar and fat intake per country'
+                    text: 'Relation between establishments number, number of beds and number of nights spent in European countries'
                 },
 
                 subtitle: {
-                    text: 'Source: <a href="http://www.euromonitor.com/">Euromonitor</a> and <a href="https://data.oecd.org/">OECD</a>'
+                    text: 'Source: <a href="http://ec.europa.eu/eurostat/statistics-explained/index.php/File:Tourist_accommodation_establishments,_2014_YB16.png">Eurostat</a>'
                 },
 
                 xAxis: {
                     gridLineWidth: 1,
                     title: {
-                        text: 'Daily fat intake'
-                    },
-                    labels: {
-                        format: '{value} gr'
+                        text: 'Number of beds'
                     },
                     plotLines: [{
                         color: 'black',
                         dashStyle: 'dot',
                         width: 2,
-                        value: 65,
+                        value: averageBeds,
                         label: {
                             rotation: 0,
                             y: 15,
                             style: {
                                 fontStyle: 'italic'
                             },
-                            text: 'Safe fat intake 65g/day'
+                            text: 'Average number of beds'
                         },
                         zIndex: 3
                     }]
@@ -178,23 +192,20 @@ angular
                     startOnTick: false,
                     endOnTick: false,
                     title: {
-                        text: 'Daily sugar intake'
-                    },
-                    labels: {
-                        format: '{value} gr'
+                        text: 'Number of nights spent'
                     },
                     maxPadding: 0.2,
                     plotLines: [{
                         color: 'black',
                         dashStyle: 'dot',
                         width: 2,
-                        value: 50,
+                        value: averageNights,
                         label: {
                             align: 'right',
                             style: {
                                 fontStyle: 'italic'
                             },
-                            text: 'Safe sugar intake 50g/day',
+                            text: 'Average number of nights spent',
                             x: -10
                         },
                         zIndex: 3
@@ -205,9 +216,9 @@ angular
                     useHTML: true,
                     headerFormat: '<table>',
                     pointFormat: '<tr><th colspan="2"><h3>{point.country}</h3></th></tr>' +
-                        '<tr><th>Fat intake:</th><td>{point.x}g</td></tr>' +
-                        '<tr><th>Sugar intake:</th><td>{point.y}g</td></tr>' +
-                        '<tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>',
+                        '<tr><th>Establishments:</th><td>{point.x}g</td></tr>' +
+                        '<tr><th>Beds:</th><td>{point.y}g</td></tr>' +
+                        '<tr><th>Nights spent:</th><td>{point.z}%</td></tr>',
                     footerFormat: '</table>',
                     followPointer: true
                 },
@@ -221,99 +232,8 @@ angular
                     }
                 },
 
-                series: [{
-                    data: [{
-                        x: 95,
-                        y: 95,
-                        z: 13.8,
-                        name: 'BE',
-                        country: 'Belgium'
-                    }, {
-                        x: 86.5,
-                        y: 102.9,
-                        z: 14.7,
-                        name: 'DE',
-                        country: 'Germany'
-                    }, {
-                        x: 80.8,
-                        y: 91.5,
-                        z: 15.8,
-                        name: 'FI',
-                        country: 'Finland'
-                    }, {
-                        x: 80.4,
-                        y: 102.5,
-                        z: 12,
-                        name: 'NL',
-                        country: 'Netherlands'
-                    }, {
-                        x: 80.3,
-                        y: 86.1,
-                        z: 11.8,
-                        name: 'SE',
-                        country: 'Sweden'
-                    }, {
-                        x: 78.4,
-                        y: 70.1,
-                        z: 16.6,
-                        name: 'ES',
-                        country: 'Spain'
-                    }, {
-                        x: 74.2,
-                        y: 68.5,
-                        z: 14.5,
-                        name: 'FR',
-                        country: 'France'
-                    }, {
-                        x: 73.5,
-                        y: 83.1,
-                        z: 10,
-                        name: 'NO',
-                        country: 'Norway'
-                    }, {
-                        x: 71,
-                        y: 93.2,
-                        z: 24.7,
-                        name: 'UK',
-                        country: 'United Kingdom'
-                    }, {
-                        x: 69.2,
-                        y: 57.6,
-                        z: 10.4,
-                        name: 'IT',
-                        country: 'Italy'
-                    }, {
-                        x: 68.6,
-                        y: 20,
-                        z: 16,
-                        name: 'RU',
-                        country: 'Russia'
-                    }, {
-                        x: 65.5,
-                        y: 126.4,
-                        z: 35.3,
-                        name: 'US',
-                        country: 'United States'
-                    }, {
-                        x: 65.4,
-                        y: 50.8,
-                        z: 28.5,
-                        name: 'HU',
-                        country: 'Hungary'
-                    }, {
-                        x: 63.4,
-                        y: 51.8,
-                        z: 15.4,
-                        name: 'PT',
-                        country: 'Portugal'
-                    }, {
-                        x: 64,
-                        y: 82.9,
-                        z: 31.3,
-                        name: 'NZ',
-                        country: 'New Zealand'
-                    }]
-                }]
+
+                series: [establishmentsSeries]
 
             });
         });
