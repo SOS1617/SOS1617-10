4ff6deaf3169a92ee6984372004658551c4687f1
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-04 17:01:47 +0000

    Motorcycling graphs

--- public/motorcyclings/motorcyclings-graph.js
+++ public/motorcyclings/motorcyclings-graph.js
@@ -13,5 +13,45 @@ angular
                 return x.country;
             }));
             console.log(countries);
-        })
-}])
+        });
+        
+        
+        
+    Highcharts.chart('container', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45
+        }
+    },
+    title: {
+        text: 'Contents of Highsoft\'s weekly fruit delivery'
+    },
+    subtitle: {
+        text: '3D donut in Highcharts'
+    },
+    plotOptions: {
+        pie: {
+            innerSize: 100,
+            depth: 45
+        }
+    },
+    series: [{
+        name: 'Delivered amount',
+        data: [
+            ['Bananas', 8],
+            ['Kiwi', 3],
+            ['Mixed nuts', 1],
+            ['Oranges', 6],
+            ['Apples', 8],
+            ['Pears', 4],
+            ['Clementines', 4],
+            ['Reddish (bag)', 1],
+            ['Grapes (bunch)', 1]
+        ]
+    }]
+});
+
+
+}]);
