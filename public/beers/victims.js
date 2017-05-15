7b262cd81c4e81a751278bd33525500058249154
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-15 19:34:20 +0000

    victims

--- public/beers/victims.js
+++ public/beers/victims.js
@@ -2,4 +2,66 @@ angular
     .module("SOS161710")
     .controller("Victims", ["$http", function ($http){
         console.log("Controller initialized!");
+        var beers = [];
+        var victims= [];
+        var yearfrom = 1900
+         /*$http.get("http://sos1617-10.herokuapp.com/api/v2/beers-stats/?from="+yearfrom+"&apikey=jesusguerre").then(function(response) {
+             var data = response.data;
+             
+             var year = Math.max(data.map( (x) => {return x.birthyear}));
+             
+             
+             data.foreach( (x) => {
+                beers.push(x.birthyear/year);
+             });
+             
+             
+             
+         });
+         
+        
+        */
+        $(function () {
+    function generateData() {
+        var d = [];
+        for (var i = 0; i < 100; i++) {
+            d.push([Date.UTC(2013, 0, i), Math.round(Math.random() * 2), Math.random()])
+        }
+        return d;
+    }
+    
+    console.log(generateData());
+
+    $('#container').highcharts({
+        chart: {
+            type: 'heatmap'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            categories: ['a', 'b', 'c']
+        },
+        colorAxis: {
+            min: 0,
+            max: 1,
+            minColor: '#a50022',
+            maxColor: '#007340',
+            gridLineColor: '#000000',
+            stops: [
+                [0, '#a50022'],
+                [0.5, '#fffbbc'],
+                [1, '#007340']
+            ],
+        },
+        series: [{
+            colsize: 24 * 36e5, // one day
+            data: generateData()
+        }]
+    });
+});
+        
+        
+        
+        
     }]);
