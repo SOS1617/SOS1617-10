b54049c2e2296ea5322008e1037306d4cc7ffe7b
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-15 10:52:59 +0000

    proxy

---
+++ public/establishments/api2.js
@@ -0,0 +1,11 @@
+angular
+    .module("SOS161710")
+    .controller("IntegrationAPI2EstablishmentsGraph", ["$http", function($http) {
+                console.log("Controller initialized");
+                var url = "http://sos1617-10.herokuapp.com/api/v2/establishments";
+                var apikey = "apikey=nurtrioje";
+                $http.get(url + "/?" + apikey).then(function(response) {
+                    
+                    
+                });
+    }]);
