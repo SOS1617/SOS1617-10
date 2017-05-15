7588ce4502aea5bac4689ea5b28e707456ba936f
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-15 21:36:53 +0000

    easter

--- public/establishments/api2.js
+++ public/establishments/api2.js
@@ -4,52 +4,52 @@ angular
         console.log("Controller initialized");
         var url = "http://sos1617-10.herokuapp.com/api/v2/establishments";
         var apikey = "apikey=nurtrioje";
-        
+
         var countries = ["Spain", "Germany", "France", "Greece"];
         var establishments = [];
         var investments = [];
         var investmentsData;
         var establishmentsData;
-        
-        $http.get("http://sos1617-03.herokuapp.com/api/v1/investmentseducation/?apikey=apisupersecreta").then(function(response){
-            investmentsData = response.data;
-            
-            countries.forEach((country) => {
-                var exist = false;
-                investmentsData.forEach((d) => {
-                    if(d.country.toLowerCase() == country.toLowerCase() && exist == false){
-                        investments.push(Number(d.inveducation)*1000000);
-                        exist = true;
-                    }
-                });
-                if(exist == false) {
-                    investments.push(null);
-                }
-            });
-             console.log(investments);
-            
-        });
-       
-        
+
+
         $http.get(url + "/?" + apikey).then(function(response) {
             establishmentsData = response.data;
-            
+
             countries.forEach((country) => {
                 var exist = false;
                 establishmentsData.forEach((d) => {
-                    if(d.country.toLowerCase() == country.toLowerCase() && exist == false){
+                    if (d.country.toLowerCase() == country.toLowerCase() && exist == false) {
                         establishments.push(d.nights);
                         exist = true;
                     }
                 });
-                if(exist == false){
+                if (exist == false) {
                     establishments.push(null);
                 }
-                
+
             });
-            
+
             console.log(establishments);
-            
+
+        });
+
+        $http.get("http://sos1617-03.herokuapp.com/api/v1/investmentseducation/?apikey=apisupersecreta").then(function(response) {
+            investmentsData = response.data;
+
+            countries.forEach((country) => {
+                var exist = false;
+                investmentsData.forEach((d) => {
+                    if (d.country.toLowerCase() == country.toLowerCase() && exist == false) {
+                        investments.push(Number(d.inveducation) * 1000000);
+                        exist = true;
+                    }
+                });
+                if (exist == false) {
+                    investments.push(null);
+                }
+            });
+            console.log(investments);
+
             Highcharts.chart('api2-establishments', {
 
                 title: {
