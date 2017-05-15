b6ff01f205d55190e4b7acf2e5d2f47a2b0f2117
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-15 17:52:19 +0000

    easter

--- public/establishments/api2.js
+++ public/establishments/api2.js
@@ -8,30 +8,47 @@ angular
             Highcharts.chart('api2-establishments', {
 
                 title: {
-                    text: 'Logarithmic axis demo'
+                    text: 'Solar Employment Growth by Sector, 2010-2016'
                 },
 
-                xAxis: {
-                    tickInterval: 1
+                subtitle: {
+                    text: 'Source: thesolarfoundation.com'
                 },
 
                 yAxis: {
-                    type: 'logarithmic',
-                    minorTickInterval: 0.1
+                    title: {
+                        text: 'Number of Employees'
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle'
                 },
 
-                tooltip: {
-                    headerFormat: '<b>{series.name}</b><br />',
-                    pointFormat: 'x = {point.x}, y = {point.y}'
+                plotOptions: {
+                    series: {
+                        pointStart: 2010
+                    }
                 },
 
                 series: [{
-                    data: [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
-                    pointStart: 1
+                    name: 'Installation',
+                    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
                 }, {
-                    data: [3, 4, 6, 10, 18, 34, 66, 130, 258, 514],
-                    pointStart: 3
+                    name: 'Manufacturing',
+                    data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+                }, {
+                    name: 'Sales & Distribution',
+                    data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+                }, {
+                    name: 'Project Development',
+                    data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
+                }, {
+                    name: 'Other',
+                    data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
                 }]
+
             });
 
         });
