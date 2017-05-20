a1d35a193f7e7bdd9a8d5eb9a238ad6c1d79308d
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-20 18:49:32 +0000

    m

---
+++ public/establishments/foursquare.js
@@ -0,0 +1,35 @@
+angular
+    .module("SOS161710")
+    .controller("Foursquare", ["$http", function($http) {
+        console.log("controller initialized");
+
+        $http.get("https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=44LIW13ZHFP05IZ4HWYSZUHAAWNK4JCSP30G3Y5PUV3LG5RH&client_secret=KQNCVSFKQRPQOELEN1VUCEMN5UFYV5A0XCSLSLFFVNV5ZD1O&v=20170520")
+            .then(function(response) {
+                
+                var aux = response.data.response.venues;
+                
+                var foursquareData = [];
+                foursquareData.push(['Task', 'Distance']);
+                for(var i=0; i<aux.length;i++){
+                    foursquareData.push([aux[i].name,aux[i].location.distance]);
+                }
+
+                google.charts.load('current', {
+                    'packages': ['corechart']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+
+                    var data = google.visualization.arrayToDataTable(foursquareData);
+
+                    var options = {
+                        title: 'Foursquare locations'
+                    };
+
+                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                    chart.draw(data, options);
+                }
+            });
+    }]);
