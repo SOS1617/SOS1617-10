4dfa2d0de7dda51b8eb6c078928d9140526da98d
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-23 20:08:18 +0000

    m

--- public/establishments/restcountries.js
+++ public/establishments/restcountries.js
@@ -3,7 +3,7 @@ angular
     .controller("RestCountries", ["$http", function($http) {
 
         var restcountriesData = [];
-        restcountriesData.push(['Location', 'Region', 'Population', 'Area'],
+        restcountriesData.push(['Location', 'Region', 'Area', 'Population'],
             ['Global', null, 0, 0], 
             ['Americas', 'Global', 0, 0],
             ['Europe', 'Global', 0, 0], 
@@ -17,9 +17,9 @@ angular
             var aux = response.data;
             aux.forEach((d) => {
                 if(!d.region){
-                    restcountriesData.push([d.name, "Global", Number(d.population), Number(d.area)]);
+                    restcountriesData.push([d.name, "Global", 0, 0]);
                 } else {
-                    restcountriesData.push([d.name, d.region, Number(d.population), Number(d.area)]);
+                    restcountriesData.push([d.name, d.region, Number(d.area), Number(d.population)]);
                 }
                 
             });
