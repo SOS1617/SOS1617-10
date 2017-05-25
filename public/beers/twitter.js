7b66d44751944ee676c3e8f96c96907c264d0257
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-25 20:40:22 +0000

    twitter view

--- public/beers/twitter.js
+++ public/beers/twitter.js
@@ -6,9 +6,9 @@ angular
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
+          ['Name', 'Sales', 'Expenses', 'Profit'],
+          ['Cruzcampo', 1000, 400, 200],
+          ['Estrella damm', 1170, 460, 250],
           ['2016', 660, 1120, 300],
           ['2017', 1030, 540, 350]
         ]);
