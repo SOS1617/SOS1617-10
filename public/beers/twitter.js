d897956faf2abe606173e21d5bd65ee91c5b246e
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-25 20:32:58 +0000

    twitter view

--- public/beers/twitter.js
+++ public/beers/twitter.js
@@ -7,36 +7,21 @@ angular
         google.charts.setOnLoadCallback(drawStuff);
 
         function drawStuff() {
-            var data = new google.visualization.arrayToDataTable([
-                ['Move', 'Percentage'],
-                ["King's pawn (e4)", 44],
-                ["Queen's pawn (d4)", 31],
-                ["Knight to King 3 (Nf3)", 12],
-                ["Queen's bishop pawn (c4)", 10],
-                ['Other', 3]
-            ]);
+            var data = google.visualization.arrayToDataTable([
+        ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+         'Western', 'Literature', { role: 'annotation' } ],
+        ['2010', 10, 24, 20, 32, 18, 5, ''],
+        ['2020', 16, 22, 23, 30, 16, 9, ''],
+        ['2030', 28, 19, 29, 30, 12, 13, '']
+      ]);
 
-            var options = {
-                width: 800,
-                legend: {
-                    position: 'none'
-                },
-                chart: {
-                    title: 'Chess opening moves',
-                    subtitle: 'popularity by percentage'
-                },
-                axes: {
-                    x: {
-                        0: {
-                            side: 'top',
-                            label: 'White to move'
-                        } // Top x-axis.
-                    }
-                },
-                bar: {
-                    groupWidth: "90%"
-                }
-            };
+      var options = {
+        width: 600,
+        height: 400,
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        isStacked: true,
+      };
 
             var chart = new google.charts.Bar(document.getElementById('top_x_div'));
             // Convert the Classic options to Material options.
