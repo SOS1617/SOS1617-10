47f3041b4e973070a321fd665ee275d7eb8d34bd
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-26 19:40:12 +0000

    analytcs

--- public/analytics.js
+++ public/analytics.js
@@ -1,5 +1,63 @@
 angular
     .module("SOS161710")
-    .controller("GroupAnalytics",["$http",function ($http){
+    .controller("GroupAnalytics", ["$http", function($http) {
         console.log("Group Analytics initialized");
+        var beersbycountry = [];
+        var dataToGraph=[];
+        dataToGraph.push(["Country","beers","establishments","motorcyclings"]);
+        var countries= ["Spain","France","Germany","Italy"];
+        var bee=[4,5,3,2]
+
+        $http.get("/api/v2/beers-stats/?apikey=jesusguerre").then(function(response) {
+            var beers=response.data;
+            
+            countries.forEach( (x)=>{
+                var cont=0;
+                beers.forEach( (y)=>{
+                    if (y.country==x){
+                        cont++;
+                    }
+                });
+                beersbycountry.push(cont);
+                
+            });
+            
+            show();
+        });
+        
+        
+        for(var index=0;index<countries.length;index++){
+            dataToGraph.push([countries[index],beersbycountry[index],0,0]);
+        }
+        
+        
+        
+        
+        
+        function show() {
+
+            google.charts.load('current', {
+                'packages': ['corechart']
+            });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable(dataToGraph);
+
+                var options = {
+                    title: 'The decline of \'The 39 Steps\'',
+                    vAxis: {
+                        title: 'Accumulated Rating'
+                    },
+                    isStacked: true
+                };
+
+                var chart = new google.visualization.SteppedAreaChart(document.getElementById('chart_div'));
+
+                chart.draw(data, options);
+            }
+        }
+
+
+
     }]);
