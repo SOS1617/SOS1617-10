67d914b39765aacb89bc16977c789e9f0cc8caa9
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-28 14:37:25 +0000

    instagram

--- public/beers/instagram.js
+++ public/beers/instagram.js
@@ -1,13 +1,37 @@
 angular
     .module("SOS161710")
-    .controller("InstaGraph",["$http","$scope", function($http,$scope){
-       
-        
-        $scope.login= function (){
-          window.location.replace("http://sos1617-10.herokuapp.com/authorize_user");  
-         
+    .controller("InstaGraph", ["$http", "$scope", function($http, $scope) {
+
+
+        $scope.login = function() {
+            window.location.replace("http://sos1617-10.herokuapp.com/authorize_user");
+
         };
-      
-        
-        
+       
+
+
+
+        var trace1 = {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [20, 14, 23],
+            name: 'SF Zoo',
+            type: 'bar'
+        };
+
+        var trace2 = {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [12, 18, 29],
+            name: 'LA Zoo',
+            type: 'bar'
+        };
+
+        var data = [trace1, trace2];
+
+        var layout = {
+            barmode: 'stack'
+        };
+
+        Plotly.newPlot('myDiv', data, layout);
+
+
     }]);
