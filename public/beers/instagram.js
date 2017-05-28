840525d635fe4006807458b1faad1fdcd9185df2
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-28 13:58:39 +0000

    instagram

--- public/beers/instagram.js
+++ public/beers/instagram.js
@@ -1,11 +1,15 @@
 angular
     .module("SOS161710")
-    .controller("InstaGraph",["$http","$scope", function($http,$scope){
-        var logged = false;
+    .controller("InstaGraph",["$http","$scope","$rootScope", function($http,$scope,$rootScope){
+        var logged=false;
+         if ($rootScope.data) {
+            logged = $rootScope.data.logstatus;
+            
+        }
         
         $scope.login= function (){
           window.location.replace("http://sos1617-10.herokuapp.com/authorize_user");  
-          logged=true;
+          $rootScope.data={logstatus:true};
         };
         if(logged==true){
             console.log("Congratulations!! You have logged in.");
