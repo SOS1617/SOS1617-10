48364f42a1502d37e0b1bc60180a7f30a77b4ca1
sos161710jdsg <jesusdavidsegoviaguerrero@gmail.com>
2017-05-28 13:52:28 +0000

    instagram

--- public/beers/instagram.js
+++ public/beers/instagram.js
@@ -1,10 +1,15 @@
 angular
     .module("SOS161710")
     .controller("InstaGraph",["$http","$scope", function($http,$scope){
+        var logged = false;
         
         $scope.login= function (){
           window.location.replace("http://sos1617-10.herokuapp.com/authorize_user");  
+          logged=true;
         };
+        if(logged==true){
+            console.log("Congratulations!! You have logged in.");
+        }
         
         
     }]);
