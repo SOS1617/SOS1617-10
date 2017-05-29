8a42836104bce1ab8fa0ba954ca1e09dc9088e20
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-29 20:26:30 +0000

    Integrations

--- public/motorcyclings/lastfm.js
+++ public/motorcyclings/lastfm.js
@@ -26,46 +26,29 @@ angular
                     lastfmData.push([aux[i].name, aux[i].playcount]);
                 }
 
-                google.charts.load('current', {packages: ['corechart', 'bar']});
-                google.charts.setOnLoadCallback(drawAnnotations);
-                
-                function drawAnnotations() {
-                      var data = google.visualization.arrayToDataTable(lastfmData);
-                
-                      var data = google.visualization.arrayToDataTable(lastfmData);
-                
-                      var options = {
-                        title: 'Population of Largest U.S. Cities',
-                        chartArea: {width: '50%'},
-                        annotations: {
-                          alwaysOutside: true,
-                          textStyle: {
-                            fontSize: 12,
-                            auraColor: 'none',
-                            color: '#555'
-                          },
-                          boxStyle: {
-                            stroke: '#ccc',
-                            strokeWidth: 1,
-                            gradient: {
-                              color1: '#f3e5f5',
-                              color2: '#f3e5f5',
-                              x1: '0%', y1: '0%',
-                              x2: '100%', y2: '100%'
-                            }
-                          }
-                        },
-                        hAxis: {
-                          title: 'Number',
-                          minValue: 0,
-                        },
-                        vAxis: {
-                          title: 'Name'
-                        }
-                      };
-                      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-                      chart.draw(data, options);
-                    }
+            google.charts.load("current", {packages:["corechart"]});
+                google.charts.setOnLoadCallback(drawChart);
+                function drawChart() {
+                  var data = google.visualization.arrayToDataTable(lastfmData);
+            
+                  var view = new google.visualization.DataView(data);
+                  view.setColumns([0, 1,
+                                   { calc: "stringify",
+                                     sourceColumn: 1,
+                                     type: "string",
+                                     role: "annotation" },
+                                   2]);
+            
+                  var options = {
+                    title: "Density of Precious Metals, in g/cm^3",
+                    width: 600,
+                    height: 400,
+                    bar: {groupWidth: "95%"},
+                    legend: { position: "none" },
+                  };
+                  var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+                  chart.draw(view, options);
+              }
             });
     
     
