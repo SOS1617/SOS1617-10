43a2aef99b92c38f4fd05247e864106811b20826
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-29 20:52:01 +0000

    Integrations

--- public/motorcyclings/lastfm.js
+++ public/motorcyclings/lastfm.js
@@ -1,59 +1,105 @@
 angular
-    .module("SOS161710")
-    .controller("Lastfm", ["$http", function($http) {
-        console.log("controller initialized");
-        var motorcyclingsCountry = [];
-        var country = ["United Kingdom", "Italy", "Spain"];
-        var lastfmData = [];
-        lastfmData.push(['Name', 'Number']);
-
-        $http.get("http://sos1617-10.herokuapp.com/api/v2/motorcycling-stats?apikey=davbotcab")
-            .then(function(response) {
-                var countries = new Set(response.data.map(function(x){
-                    return x.country;
-                }));
-                countries.forEach((country) => {
-                    motorcyclingsCountry.push(getFromCountry(country, response.data));
-                });
-            });
-
-        $http.get("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=d9c117e532f10f87c74f225f43a7f843&user=DeiVi92&format=json")
-            .then(function(response) {
-
-                var aux = response.data.artists.artist;
-
-                for (var i = 0; i < aux.length; i++) {
-                    lastfmData.push([aux[i].name, aux[i].playcount]);
-                }
-
-                google.charts.load('current', {
-                    'packages': ['corechart']
-                });
-                google.charts.setOnLoadCallback(drawChart);
-
-                function drawChart() {
-
-                    var data = google.visualization.arrayToDataTable(lastfmData);
-
-                    var options = {
-                        title: 'LastFM locations integrated with Motorcyclings'
-                    };
-
-                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-                    chart.draw(data, options);
-                }
-            });
+.module("SOS161710")
+.controller("Lastfm", ["$http", function($http){
+    console.log("Controller initialized");
+    var url = "http://sos1617-10.herokuapp.com/api/v2/motorcycling-stats";
+    var apikey = "apikey=davbotcab";
+    var lastfmData = {};
+    var name = [];
+    var playcount = [];
+        
+    var motorcyclingsCountry = [];
     
+    $http.get(url + "/?" + apikey).then(function(response){
+        var countries = new Set(response.data.map(function(x){
+            return x.country;
+        }));
+        countries.forEach((country) => {
+            motorcyclingsCountry.push(getFromCountry(country, response.data));
+            
+        });
+    });
     
-    function getFromCountry(country, data) {
+        
+    $http.get("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=d9c117e532f10f87c74f225f43a7f843&user=DeiVi92&format=json").then(function(response){
+        
+        lastfmData = response.data.artists.artist;
+                
+        for(var i=0; i<lastfmData.length; i++){
+            name.push(lastfmData[i].name);
+            playcount.push(Number(playcount[i]["playcount"]));
+        }
+        
+        
+    Highcharts.chart('container', {
+        chart: {
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Lastfm ft Motorcyclings'
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            categories: name
+        },
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series:[{   type: 'area',
+                    name: 'Motorcyclings',
+                    data: motorcyclingsCountry
+                }, {
+                    type: 'area',
+                    name: 'playcount',
+                    data: playcount
+         }]
+    });
+
+    
+});
+
+function getFromCountry(country, data) {
         var response;
         response = [country, data.filter((x) => {
             return x.country == country;
         }).length];
         return response;
     }
+
     
 }]);
-
-
