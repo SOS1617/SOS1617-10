41d4f07bdd04c055d9de7e5d2913d388284a2512
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-29 22:07:37 +0000

    Integrations

--- public/motorcyclings/lastfm.js
+++ public/motorcyclings/lastfm.js
@@ -8,14 +8,13 @@ angular
     var name = [];
     var playcount = [];
         
-    var motorcyclingsCountry = [];
     
     $http.get(url + "/?" + apikey).then(function(response){
         var countries = new Set(response.data.map(function(x){
             return x.country;
         }));
         countries.forEach((country) => {
-            motorcyclingsCountry.push(getFromCountry(country, response.data));
+            lastfmData.push(getFromCountry(country, response.data));
             
         });
     });
@@ -23,72 +22,26 @@ angular
         
     $http.get("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=d9c117e532f10f87c74f225f43a7f843&user=DeiVi92&format=json").then(function(response){
         
-        lastfmData = response.data.artists.artist;
+        var aux = response.data.artists.artist;
                 
         for(var i=0; i<lastfmData.length; i++){
-            name.push(lastfmData[i].name);
-            playcount.push(Number(lastfmData[i].playcount));
+            lastfmData.push([aux[i].name, aux[i].playcount]);
         }
         
         
-    Highcharts.chart('container', {
-        chart: {
-            zoomType: 'x'
-        },
-        title: {
-            text: 'Lastfm ft Motorcyclings'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            categories: name
-        },
-        yAxis: {
-            title: {
-                text: 'Exchange rate'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                fillColor: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
-                    },
-                    stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                    ]
-                },
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
-            }
-        },
+         google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(lastfmData);
 
-        series:[{   type: 'area',
-                    name: 'Motorcyclings',
-                    data: motorcyclingsCountry
-                }, {
-                    type: 'area',
-                    name: 'playcount',
-                    data: playcount
-         }]
-    });
+        var options = {
+          title: 'My Daily Activities',
+          pieHole: 0.4,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart.draw(data, options);
+      }
 
     
 });
