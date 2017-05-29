82eb17405b5449cde492492670594227c192150c
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-29 20:46:28 +0000

    Integrations

--- public/motorcyclings/lastfm.js
+++ public/motorcyclings/lastfm.js
@@ -2,31 +2,31 @@ angular
     .module("SOS161710")
     .controller("Lastfm", ["$http", function($http) {
         console.log("controller initialized");
-        var motorcyclingsPilot = [];
-        var pilot = ["Jorge Lorenzo", "Valentino Rossi"];
+        var motorcyclingsCountry = [];
+        var country = ["United Kingdom", "Italy", "Spain"];
         var lastfmData = [];
         lastfmData.push(['Name', 'Number']);
 
         $http.get("http://sos1617-10.herokuapp.com/api/v2/motorcycling-stats?apikey=davbotcab")
             .then(function(response) {
-                var pilots = new Set(response.data.map(function(x){
-                    return x.pilot;
+                var countries = new Set(response.data.map(function(x){
+                    return x.country;
                 }));
-                pilots.forEach((pilot) => {
-                    motorcyclingsPilot.push(getFromPilot(pilot, response.data));
+                countries.forEach((country) => {
+                    motorcyclingsCountry.push(getFromCountry(country, response.data));
                 });
             });
 
         $http.get("http://ws.audioscrobbler.com/2.0/?method=library.getartists&api_key=d9c117e532f10f87c74f225f43a7f843&user=DeiVi92&format=json")
             .then(function(response) {
 
-                var aux = response.data.artists.artist;
+                var aux = response.data.response.artists.artist;
 
                 for (var i = 0; i < aux.length; i++) {
                     lastfmData.push([aux[i].name, aux[i].playcount]);
                 }
 
-             google.charts.load('current', {
+                google.charts.load('current', {
                     'packages': ['corechart']
                 });
                 google.charts.setOnLoadCallback(drawChart);
@@ -36,7 +36,7 @@ angular
                     var data = google.visualization.arrayToDataTable(lastfmData);
 
                     var options = {
-                        title: 'Lastfm integrated with Motorcyclings'
+                        title: 'LastFM locations integrated with Motorcyclings'
                     };
 
                     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -46,10 +46,10 @@ angular
             });
     
     
-    function getFromPilot(country, data) {
+    function getFromCountry(country, data) {
         var response;
-        response = [pilot, data.filter((x) => {
-            return x.pilot == pilot;
+        response = [country, data.filter((x) => {
+            return x.country == country;
         }).length];
         return response;
     }
