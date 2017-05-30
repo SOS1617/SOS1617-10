c6ddeba04187b4f74358dc771b859efa1f3de756
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-30 08:51:12 +0000

    Integrations

--- public/motorcyclings/lastfm.js
+++ public/motorcyclings/lastfm.js
@@ -17,22 +17,127 @@ angular
             playcount.push(res[i].playcount);
         }
         
-        var chartData = {
-      type: "bar",  // Specify your chart type here.
-      title: {
-        text: "My First Chart" // Adds a title to your chart
-      },
-      legend: {}, // Creates an interactive legend
-      series: [  // Insert your series data here.
-          { name: [03]}
-      ]
-    };
-    zingchart.render({ // Render Method[3]
-      id: "chartDiv",
-      data: chartData,
-      height: 400,
-      width: 600
-    });
+            //ZingChart
+            var myChart = {
+                "type": "line",
+                
+                "backgroundColor":'#FFFFFF',
+                "title": {
+                    "text": "Id analytics",
+                    "fontColor":"#E3E3E5",
+                    "font-size": "24px",
+                    "adjust-layout": true
+                },
+                "plotarea": {
+                    "margin": "dynamic 45 60 dynamic",
+                },
+                
+                "legend": {
+                    "layout": "float",
+                    "background-color": "none",
+                    "border-width": 0,
+                    "shadow": 0,
+                    "align": "center",
+                    "adjust-layout": true,
+                "item": {
+                    "padding": 7,
+                    "marginRight": 17,
+                    "cursor": "hand"
+                }
+                },
+                
+                "scale-x": {
+                    "label": {
+                        "text": "Types",
+                        "fontColor":"#E3E3E5",
+
+                    },
+                    "labels": 
+                        name
+                    
+                },
+                "scale-y": {
+                    "min-value": "5954375400:1383292800000",
+                    "label": {
+                        "text": "Ids",
+                        "fontColor":"#E3E3E5",
+
+                    },
+                    
+                },
+                
+                "crosshair-x": {
+                    "line-color": "#efefef",
+                    "plot-label": {
+                    "border-radius": "5px",
+                    "border-width": "1px",
+                    "border-color": "#f6f7f8",
+                    "padding": "10px",
+                    "font-weight": "bold"
+                },
+                "scale-label": {
+                    "font-color": "#000",
+                    "background-color": "#f6f7f8",
+                    "border-radius": "5px"
+                }
+            },
+                
+                "tooltip": {
+                    "visible": false
+                },
+                
+                "plot": {
+                    "highlight": true,
+                    "tooltip-text": "%t views: %v<br>%k",
+                    "shadow": 0,
+                    "line-width": "2px",
+                    "marker": {
+                    "type": "circle",
+                    "size": 3
+                },
+                "highlight-state": {
+                "line-width": 3
+                },
+                "animation": {
+                    "effect": 1,
+                    "sequence": 2,
+                    "speed": 100,
+                }
+                },
+                
+                "series": [
+                {
+                    "values": playcount,
+                    "text": "Playcount",
+                    "line-color": "#007790",
+                    "legend-item":{
+                      "background-color": "#007790",
+                      "borderRadius":5,
+                      "font-color":"white"
+                    },
+                    "legend-marker": {
+                        "visible":false
+                    },
+                    "marker": {
+                        "background-color": "#007790",
+                        "border-width": 1,
+                        "shadow": 0,
+                        "border-color": "#69dbf1"
+                    },
+                    "highlight-marker":{
+                      "size":6,
+                      "background-color": "#007790",
+                    }
+                }
+            ]
+            };
+
+            zingchart.render({
+                id: 'chart',
+                data: myChart,
+                height: '100%',
+                width: '95%'
+            });
             
             
 });
