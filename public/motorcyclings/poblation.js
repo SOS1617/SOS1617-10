e86c1cb2f1a25e38c485c752c69abfd45bef2ab4
sos161710dbc <david.bote.cabeza@gmail.com>
2017-05-30 09:07:57 +0000

    Integrations

--- public/motorcyclings/poblation.js
+++ public/motorcyclings/poblation.js
@@ -6,108 +6,32 @@ angular
     var apikey = "apikey=davbotcab";
     var poblationData = [];
     poblationData.push(['Name', 'Number']);
-    var country = ["United Kingdom", "Italy", "Spain"];
-    var population = [];
-    var name = [];
     
     
         
     $http.get("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo").then(function(response){
         
-                poblationData = response.data;
+                var res = response.data.geonames;
                         
-                for(var i=0; i<response.data.length; i++){
-                    name.push(poblationData[i].name);
-                    population.push(Number(poblationData[i].population));
+                for(var i=0; i<res.length; i++){
+                    poblationData.push([res[i].countrycode, res[i].population]);
                 }
                 
         
         
-                var myChart = {
-             	type: "pie", 
-             	backgroundColor: "#2B313B",
-             	plot: {
-             	  borderColor: "#2B313B",
-             	  borderWidth: 5,
-             	  // slice: 90,
-             	  valueBox: {
-             	    placement: 'out',
-             	    text: '%t\n%npv%',
-             	    fontFamily: "Open Sans"
-             	  },
-             	  tooltip:{
-             	    fontSize: '18',
-             	    fontFamily: "Open Sans",
-             	    padding: "5 10",
-             	    text: "%npv%"
-             	  },
-             	  animation:{
-                  effect: 2, 
-                  method: 5,
-                  speed: 500,
-                  sequence: 1
-                }
-             	},
-             	source: {
-             	  text: 'gs.statcounter.com',
-             	  fontColor: "#8e99a9",
-             	  fontFamily: "Open Sans"
-             	},
-             	title: {
-             	  fontColor: "#fff",
-             	  text: 'Global Browser Usage',
-             	  align: "left",
-             	  offsetX: 10,
-             	  fontFamily: "Open Sans",
-             	  fontSize: 25
-             	},
-             	subtitle: {
-             	  offsetX: 10,
-             	  offsetY: 10,
-             	  fontColor: "#8e99a9",
-             	  fontFamily: "Open Sans",
-             	  fontSize: "16",
-             	  text: 'May 2016',
-             	  align: "left"
-             	},
-             	plotarea: {
-             	  margin: "20 0 0 0"  
-             	},
-            	series : [
-            		{
-            			values : [11.38],
-            			text: "Internet Explorer",
-            		  backgroundColor: '#50ADF5',
-            		},
-            		{
-            		  values: [56.94],
-            		  text: "Chrome",
-            		  backgroundColor: '#FF7965'
-            		},
-            		{
-            		  values: [14.52],
-            		  text: 'Firefox',
-            		  backgroundColor: '#FFCB45'
-            		},
-            		{
-            		  text: 'Safari',
-            		  values: [9.69],
-            		  backgroundColor: '#6877e5'
-            		},
-            		{
-            		  text: 'Other',
-            		  values: [7.48],
-            		  backgroundColor: '#6FB07F'
-            		}
-            	]
-            };
-             
-            zingchart.render({ 
-            	id : 'myChart', 
-            	data : myChart, 
-            	height: 500, 
-            	width: 725 
-            });
+              google.charts.load("current", {packages:["corechart"]});
+              google.charts.setOnLoadCallback(drawChart);
+              function drawChart() {
+                var data = google.visualization.arrayToDataTable(poblationData);
+        
+                var options = {
+                  title: 'Poblation Graph',
+                  pieHole: 0.9,
+                };
+        
+                var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+                chart.draw(data, options);
+              }
 
     
 });
