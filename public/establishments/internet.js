65f1ec565ce2730a31cdb4574022c99f34d8e29e
sos161710nto <nuria.t.ojeda@gmail.com>
2017-05-30 20:48:37 +0000

    m

--- public/establishments/internet.js
+++ public/establishments/internet.js
@@ -12,7 +12,7 @@ angular
 
             response.data.forEach((x) => {
                 countriesEstablishments.push(x.country);
-                numberEstablishments.push(x.number);
+                numberEstablishments.push(x.number/1000);
             });
 
         });
@@ -28,7 +28,7 @@ angular
             var trace1 = {
                 x: countriesEstablishments,
                 y: numberEstablishments,
-                name: 'Number of establishments',
+                name: 'Number of establishments per 1000',
                 type: 'bar'
             };
 
